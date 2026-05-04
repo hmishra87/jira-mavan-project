50d4b81e6d7750f3c22c1c33881820e84f3a8d98
Ubuntu <ubuntu@ip-172-31-22-75.ec2.internal>
2026-05-04 07:24:40 +0000

    my-first commit

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 DevOps Pipeline Success";
+    }
+}
